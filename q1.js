936336bb08ea6059caa5482a0d1c4c3c341b1dc4
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-05-25 21:27:33 +0530

    commited

--- q1.js
+++ q1.js
@@ -22,3 +22,50 @@ function longestSubarrayWithSumK(arr, k) {
 let arr=[9,-8,5,4,-1,2,1,-5];
 let k=6;
 console.log(longestSubarrayWithSumK(arr,k));
+
+function topKFrequent(nums, k) {
+  const freqMap = new Map();
+  for (let num of nums) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+
+  const bucket = Array(nums.length + 1).fill().map(() => []);
+  for (let [num, freq] of freqMap) {
+    bucket[freq].push(num);
+  }
+
+  const result = [];
+  for (let i = bucket.length - 1; i >= 0 && result.length < k; i--) {
+    for (let num of bucket[i]) {
+      result.push(num);
+      if (result.length === k) break;
+    }
+  }
+
+  return result;
+}
+
+console.log(topKFrequent(arr,k));
+
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let count = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        count++;
+      }
+
+      longest = Math.max(longest, count);
+    }
+  }
+
+  return longest;
+}
+
+console.log(longestConsecutive(arr));
